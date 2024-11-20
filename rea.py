8b16a445dbc4d9ba0b32bbf33d19cf1e92e47767
msanch023 <mas023@ucsd.edu>
2024-11-19 17:18:13 -0800

    little edit

--- rea.py
+++ rea.py
@@ -4,13 +4,12 @@ import numpy as np
 from scipy import interpolate
 from scipy.stats import skew
 
-def rsa(waveform, tp0):
-    
+def rea(waveform, tp0):
+
     end = np.where(waveform == np.max(waveform))[0][0]
 
     interp_range = np.arange(tp0, end + 1, 1)
     interp = interpolate.interp1d(np.arange(waveform.shape[0]), waveform, kind = 'linear')
     interp_vals = interp(interp_range)
-    
-    return skew(interp_vals)
 
+    return skew(interp_vals)
