124b7b0031c135a3b3c2ec4647b70b0ce7dac54f
rdoh555 <114268068+rdoh555@users.noreply.github.com>
2024-12-04 20:53:48 -0800

    Update inflection.py

--- src/parameter-functions/inflection.py
+++ src/parameter-functions/inflection.py
@@ -5,7 +5,7 @@ def inflection_points(waveform, tp0):
     tp100 = np.argmax(waveform)
     tp100_val = waveform[tp100]
     tp_val = waveform[tp0] + (tp100_val * 0.8)
-    tp = np.argmin(np.abs(waveform[tp0:] - tp_val))
+    tp = np.argmin(np.abs(waveform[tp0:tp100] - tp_val))
     tp80 = tp + tp0
     threshold = waveform[tp80]
 
