6559f34e680df3f53b244d993a0110db6a3cfef3
rdoh555 <114268068+rdoh555@users.noreply.github.com>
2024-12-04 21:10:58 -0800

    Update lq80.py

--- src/parameter-functions/lq80.py
+++ src/parameter-functions/lq80.py
@@ -5,10 +5,7 @@ import pandas as pd
 def LQ80(waveform, tp0):
     tp100 = np.argmax(waveform)
     tp100_val = waveform[tp100]
-    tp_val = waveform[tp0] + (tp100_val * 0.8)
-    tp = np.argmin(np.abs(waveform[tp0:tp100] - tp_val))
-    tp80 = tp + tp0
-    threshold = waveform[tp80]
-    crossing_index = np.where(waveform >= threshold)[0][-1]
-    charge_after_80 = np.sum(waveform[crossing_index:])
+    tp_val = (tp100_val * 0.8)
+    tp80 = np.argmin(np.abs(waveform[tp0:tp100] - tp_val))
+    charge_after_80 = np.sum(waveform[tp80:tp100])
     return charge_after_80
