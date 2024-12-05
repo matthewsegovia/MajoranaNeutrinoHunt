d0f1e3af483006c16965b73c028eec0ba1e6f514
rdoh555 <114268068+rdoh555@users.noreply.github.com>
2024-12-04 20:52:50 -0800

    Update lq80.py

--- src/parameter-functions/lq80.py
+++ src/parameter-functions/lq80.py
@@ -6,7 +6,7 @@ def LQ80(waveform, tp0):
     tp100 = np.argmax(waveform)
     tp100_val = waveform[tp100]
     tp_val = waveform[tp0] + (tp100_val * 0.8)
-    tp = np.argmin(np.abs(waveform[tp0:] - tp_val))
+    tp = np.argmin(np.abs(waveform[tp0:tp100] - tp_val))
     tp80 = tp + tp0
     threshold = waveform[tp80]
     crossing_index = np.where(waveform >= threshold)[0][-1]
