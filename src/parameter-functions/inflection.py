826379b7198bb4d2c3d911c309b1c29411620825
rdoh555 <114268068+rdoh555@users.noreply.github.com>
2024-12-04 22:18:09 -0800

    Update inflection.py

--- src/parameter-functions/inflection.py
+++ src/parameter-functions/inflection.py
@@ -4,12 +4,14 @@ import pandas as pd
 def inflection_points(waveform, tp0):
     tp100 = np.argmax(waveform)
     tp100_val = waveform[tp100]
-    tp_val = waveform[tp0] + (tp100_val * 0.8)
-    tp = np.argmin(np.abs(waveform[tp0:tp100] - tp_val))
-    tp80 = tp + tp0
-    threshold = waveform[tp80]
-
-    crossing_index = np.where(waveform >= threshold)[0][0]
+    if tp0 < 0:
+        tp0 = abs(tp0)
+    if (tp100 <= tp0):
+        return 0
+    tp_val = (tp100_val * 0.8)
+    tp80 = np.argmin(np.abs(waveform[tp0:tp100] - tp_val))
+    
+    crossing_index = tp80
     LQ80 = waveform[crossing_index:tp100]
     if len(LQ80) < 2:
         return 0
